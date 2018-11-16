c7a39a47991ad3893655ca64ebdb852366d1ec4d
Student <example@example.com>
2018-11-16 08:01:38 -0800

    complete environment setup

---
+++ spec/ruby_spec.rb
@@ -0,0 +1,17 @@
+require ('rspec')
+require ('pry')
+require ('Grammable')
+
+
+describe('Grammable') do 
+
+  it('tests a method for Grammable') do
+
+    dummy = Grammable.new()
+
+    expect(dummy.method()).to(eq(expected result))
+
+  end
+
+end
+
