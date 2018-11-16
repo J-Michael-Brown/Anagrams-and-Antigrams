0b2cee830e786d960cb8f0203d612a21442e1fa5
Student <example@example.com>
2018-11-16 08:23:07 -0800

    add class to store two words and perform methods readme template

--- lib/Grammable.rb
+++ lib/Grammable.rb
@@ -1,5 +1,18 @@
 class Grammable
+  def initialize(word1="live", word2="evil")
+    @word1 = word1
+    @word2 = word2
+  end
 
+  def change_first(new_word)
+    @word1 = new_word
+  end
+
+  def change_second(new_word)
+    @word2 = new_word
+  end
+
+  def display_words()
+    "'#{@word1}' and '#{@word2}'"
+  end
 end
-
-
